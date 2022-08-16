007c3beae1487e3d20c625e634c613b13e16637d
Chad Chapman <chad.chapman2010@gmail.com>
2022-08-15 21:27:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/******************************************************************************
+ *          Name: index.php
+ *        AUTHOR: Chad Chapman
+ *  DATE CREATED: 20200810
+ *   DESCRIPTION: Top most level home page that proviedes links to all other
+ *                web pages.
+ *       VERSION: 1.0
+ *****************************************************************************/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="src/css/styles.css"/>
+        <title>Welcome to Chad Chapman's Website</title>
+    </head>
+
+    <body>
+        <h1>Welcome to Chad Chapman's Website</h1>
+
+        <div>
+            <h2>Sample Sites</h2>
+            <ul>
+                <li><a href="/sites-available/gamegallery/html/gamegallery.php">Video Game Gallery</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
